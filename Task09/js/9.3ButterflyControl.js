c979a8e33753b0fd1a19506577737321cca10a2e
gruled <fysiks@yandex.ru>
2020-02-04 12:17:02 +0400

    Add 9.3

--- Task09/js/9.3ButterflyControl.js
+++ Task09/js/9.3ButterflyControl.js
@@ -1,11 +1,31 @@
-//alert("hello");
-function hhe() {
-    var objSel = document.firstForm.firstSelect;
-    var objSelSec = document.secondForm.secondSelect;
+function hhe(inSelect, outSelect) {
+    var objSel = document.getElementById(outSelect);
+    var objSelSec = document.getElementById(inSelect);
     var optionSel = objSel.options;
-    for (let i = 0; i < optionSel.length; i++) {
-        if(optionSel[i].selected){
-            objSelSec.options[objSelSec.options.length] = optionSel[i];
+    var arr = getSelectedIndexes(objSel);
+    if(arr.length>0){
+        for (let i = 0; i < arr.length; i++) {
+            var s = getSelectedIndexes(objSel);
+            optionSel[s[0]].selected=false;
+            objSelSec.options[objSelSec.options.length] = optionSel[s[0]];
         }
+    }
+    else{
+        alert("empty output");
+    }
+}
+function getSelectedIndexes(oListbox) {
+    var arrIndexes = new Array;
+    for (var i = 0; i < oListbox.options.length; i++) {
+        if (oListbox.options[i].selected) arrIndexes.push(i);
+    }
+    return arrIndexes;
+}
+function hhq(inSelect, outSelect) {
+    var objSel = document.getElementById(outSelect);
+    var objSelSec = document.getElementById(inSelect);
+    var objSelLength = objSel.options.length;
+    for (let i = 0; i < objSelLength; i++) {
+        objSelSec.options[objSelSec.options.length] = objSel.options[0];
     }
 }
